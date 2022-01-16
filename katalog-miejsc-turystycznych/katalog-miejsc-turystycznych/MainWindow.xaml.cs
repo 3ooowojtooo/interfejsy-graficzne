7044daa3165fdaed5477ad09c7f0f2e85a170315
Wojciech Góralczyk <goralczyk4@gmail.com>
2022-01-16 23:21:03 +0100

    Delete test code from window

--- katalog-miejsc-turystycznych/katalog-miejsc-turystycznych/MainWindow.xaml.cs
+++ katalog-miejsc-turystycznych/katalog-miejsc-turystycznych/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using core;
 
 namespace katalog_miejsc_turystycznych
 {
@@ -24,9 +23,6 @@ namespace katalog_miejsc_turystycznych
         public MainWindow()
         {
             InitializeComponent();
-            PlacesManager.GetInstance().AddPlace("name11", "description11", new List<double>(), "localizaton11", new List<InterestingPlace>());
-            var places = PlacesManager.GetInstance().Places;
-            Console.WriteLine(places.Count);
         }
     }
 }
