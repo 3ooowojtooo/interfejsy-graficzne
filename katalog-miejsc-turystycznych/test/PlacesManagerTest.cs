51c606d7dcd476f16ba357c894da4a492437e39b
Wojciech Góralczyk <goralczyk4@gmail.com>
2022-01-19 19:51:43 +0100

    Finish tests. Fix place indentifier computing

--- katalog-miejsc-turystycznych/test/PlacesManagerTest.cs
+++ katalog-miejsc-turystycznych/test/PlacesManagerTest.cs
@@ -26,16 +26,19 @@ namespace test
             var interestingPlace1 = new InterestingPlace("interestingName1", "interestingLocalization1", "interestingDescription1");
             var interestingPlace2 = new InterestingPlace("interestingName2", "interestingLocalization2", "interestingDescription2");
             var reviews = new List<double> { 1.5, 2.5 };
-            var place = new Place(1, "name1", "desc1", reviews, "localization1", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var place1 = new Place(0, "name1", "desc1", reviews, "localization1", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var place2 = new Place(1, "name2", "desc2", reviews, "localization2", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
 
             FileHandlerMock.Setup(x => x.ReadFile(It.IsAny<string>())).Returns(returnedJson);
-            JsonHandlerMock.Setup(x => x.Deserialize(returnedJson)).Returns(new List<Place> { place });
+            JsonHandlerMock.Setup(x => x.Deserialize(returnedJson)).Returns(new List<Place> { place1, place2 });
 
             PlacesManager Manager = new PlacesManager(JsonHandlerMock.Object, FileHandlerMock.Object);
 
-            Assert.NotNull(Manager.Places);
-            Assert.AreEqual(1, Manager.Places.Count);
-            Assert.AreSame(place, Manager.Places[0]);
+            var places = Manager.GetPlaces();
+            Assert.NotNull(places);
+            Assert.AreEqual(2, places.Count);
+            Assert.AreSame(place1, places[0]);
+            Assert.AreSame(place2, places[1]);
             JsonHandlerMock.VerifyAll();
             FileHandlerMock.VerifyAll();
         }
@@ -79,6 +82,101 @@ namespace test
 
             PlacesManager Manager = new PlacesManager(JsonHandlerMock.Object, FileHandlerMock.Object);
             Manager.AddPlace("name2", "desc2", reviews, "localization2", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+
+            JsonHandlerMock.VerifyAll();
+            FileHandlerMock.VerifyAll();
+        }
+
+        [Test]
+        public void DeleteExistingPlaceShouldWritePlacesToFile()
+        {
+            var returnedJson = "abc";
+            var interestingPlace1 = new InterestingPlace("interestingName1", "interestingLocalization1", "interestingDescription1");
+            var interestingPlace2 = new InterestingPlace("interestingName2", "interestingLocalization2", "interestingDescription2");
+            var reviews = new List<double> { 1.5, 2.5 };
+            var place1 = new Place(0, "name1", "desc1", reviews, "localization1", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var place2 = new Place(1, "name2", "desc2", reviews, "localization2", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+
+            FileHandlerMock.Setup(x => x.ReadFile(It.IsAny<string>())).Returns(returnedJson);
+            JsonHandlerMock.Setup(x => x.Deserialize(returnedJson)).Returns(new List<Place> { place1, place2 });
+
+            JsonHandlerMock.Setup(x => x.Serialize(new List<Place> { place2 })).Returns(returnedJson);
+            FileHandlerMock.Setup(x => x.WriteToFile(It.IsAny<string>(), returnedJson));
+
+            PlacesManager Manager = new PlacesManager(JsonHandlerMock.Object, FileHandlerMock.Object);
+            Manager.DeletePlace(0);
+
+            JsonHandlerMock.VerifyAll();
+            FileHandlerMock.VerifyAll();
+        }
+
+        [Test]
+        public void DeleteNonExistingPlaceShouldNotWritePlacesToFile()
+        {
+            var returnedJson = "abc";
+            var interestingPlace1 = new InterestingPlace("interestingName1", "interestingLocalization1", "interestingDescription1");
+            var interestingPlace2 = new InterestingPlace("interestingName2", "interestingLocalization2", "interestingDescription2");
+            var reviews = new List<double> { 1.5, 2.5 };
+            var place1 = new Place(0, "name1", "desc1", reviews, "localization1", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var place2 = new Place(1, "name2", "desc2", reviews, "localization2", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+
+            FileHandlerMock.Setup(x => x.ReadFile(It.IsAny<string>())).Returns(returnedJson);
+            JsonHandlerMock.Setup(x => x.Deserialize(returnedJson)).Returns(new List<Place> { place1, place2 });
+
+            PlacesManager Manager = new PlacesManager(JsonHandlerMock.Object, FileHandlerMock.Object);
+            Manager.DeletePlace(100);
+
+            var placesAfterDelete = Manager.GetPlaces();
+            Assert.AreEqual(2, placesAfterDelete.Count);
+            Assert.AreSame(place1, placesAfterDelete[0]);
+            Assert.AreSame(place2, placesAfterDelete[1]);
+
+            JsonHandlerMock.VerifyAll();
+            FileHandlerMock.VerifyAll();
+        }
+
+        [Test]
+        public void UpdateExistingPlaceShouldWritePlacesToFile()
+        {
+            var returnedJson = "abc";
+            var interestingPlace1 = new InterestingPlace("interestingName1", "interestingLocalization1", "interestingDescription1");
+            var interestingPlace2 = new InterestingPlace("interestingName2", "interestingLocalization2", "interestingDescription2");
+            var reviews = new List<double> { 1.5, 2.5 };
+            var place1 = new Place(0, "name1", "desc1", reviews, "localization1", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var place2 = new Place(1, "name2", "desc2", reviews, "localization2", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var updatedPlace = new Place(1, "name2updated", "desc2updated", reviews, "localization2updated", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+
+            FileHandlerMock.Setup(x => x.ReadFile(It.IsAny<string>())).Returns(returnedJson);
+            JsonHandlerMock.Setup(x => x.Deserialize(returnedJson)).Returns(new List<Place> { place1, place2 });
+
+            JsonHandlerMock.Setup(x => x.Serialize(new List<Place> { place1, updatedPlace })).Returns(returnedJson);
+            FileHandlerMock.Setup(x => x.WriteToFile(It.IsAny<string>(), returnedJson));
+
+            PlacesManager Manager = new PlacesManager(JsonHandlerMock.Object, FileHandlerMock.Object);
+            Manager.Update(updatedPlace);
+
+            Assert.AreEqual(updatedPlace, Manager.GetPlace(1));
+
+            JsonHandlerMock.VerifyAll();
+            FileHandlerMock.VerifyAll();
+        }
+
+        [Test]
+        public void UpdateNotExistingPlaceShouldNotWritePlacesToFire()
+        {
+            var returnedJson = "abc";
+            var interestingPlace1 = new InterestingPlace("interestingName1", "interestingLocalization1", "interestingDescription1");
+            var interestingPlace2 = new InterestingPlace("interestingName2", "interestingLocalization2", "interestingDescription2");
+            var reviews = new List<double> { 1.5, 2.5 };
+            var place1 = new Place(0, "name1", "desc1", reviews, "localization1", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var place2 = new Place(1, "name2", "desc2", reviews, "localization2", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+            var updatedPlace = new Place(100, "name2updated", "desc2updated", reviews, "localization2updated", new List<InterestingPlace> { interestingPlace1, interestingPlace2 });
+
+            FileHandlerMock.Setup(x => x.ReadFile(It.IsAny<string>())).Returns(returnedJson);
+            JsonHandlerMock.Setup(x => x.Deserialize(returnedJson)).Returns(new List<Place> { place1, place2 });
+
+            PlacesManager Manager = new PlacesManager(JsonHandlerMock.Object, FileHandlerMock.Object);
+            Manager.Update(updatedPlace);
 
             JsonHandlerMock.VerifyAll();
             FileHandlerMock.VerifyAll();
